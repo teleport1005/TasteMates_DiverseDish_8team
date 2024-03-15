4afd354a247af2b8c74cc4dc8085a243af416bd7
teleport1005 <rabbinet@naver.com>
2024-03-15 14:20:11 +0900

    기본 프로젝트 업로드 테스트

---
+++ src/main/java/TasteMates/DiverseDish/DiverseDishApplication.java
@@ -0,0 +1,13 @@
+package TasteMates.DiverseDish;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiverseDishApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiverseDishApplication.class, args);
+	}
+
+}
---
+++ src/test/java/TasteMates/DiverseDish/DiverseDishApplicationTests.java
@@ -0,0 +1,13 @@
+package TasteMates.DiverseDish;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiverseDishApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

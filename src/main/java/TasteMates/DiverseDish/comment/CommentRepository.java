63990c308a1cd06ab7becb6ed13469c5c346e3b7
JUO-CHOI <csu970902@gmail.com>
2024-04-03 13:19:44 +0900

    레시피 삭제 시 연결된 댓글, 리뷰 삭제

--- src/main/java/TasteMates/DiverseDish/comment/CommentRepository.java
+++ src/main/java/TasteMates/DiverseDish/comment/CommentRepository.java
@@ -6,4 +6,5 @@ import java.util.List;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
     List<Comment> findByRecipe_idOrderByIdAsc(Long RecipeId);
+    void deleteByRecipe_id(Long RecipeId);
 }
